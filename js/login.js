7ea9901ecbfa56b4a26cca74062e0980a3fa568b
ssmahim01 <mahimzayn666@gmail.com>
2024-09-18 22:08:39 +0600

    Added HTML and JS file of Payoo Home page

--- js/login.js
+++ js/login.js
@@ -1,9 +1,19 @@
-// Search: form submit then loading.
-
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('login is complete');
+
+    // Get the phone Number and Pin
 
     const phoneNumber = document.getElementById('phone-num').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    // Normal way and should not use that
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Please enter the correct Number or Pin');
+    };
 });
